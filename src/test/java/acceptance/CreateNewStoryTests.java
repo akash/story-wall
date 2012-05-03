e28f5bdc66ffccd0e3cc34b28c5f24155058bbbb
Akash <akash@thoughtworks.com>
2012-05-03 16:08:36 +0100

    akash - fixing test

--- src/test/java/acceptance/CreateNewStoryTests.java
+++ src/test/java/acceptance/CreateNewStoryTests.java
@@ -5,6 +5,8 @@ import util.page.CreateNewStoryPage;
 import util.page.NavigateTo;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Matchers.notNull;
 
@@ -14,7 +16,9 @@ public class CreateNewStoryTests extends AcceptanceTestBase{
     public void shouldAllowUserToEnterTitleAndEstimate(){
         CreateNewStoryPage page = NavigateTo.createStoryPage(driver);
 
-        assertThat(page.titleField(), is(notNull()));
-        assertThat(page.estimateField(), is(notNull()));
+        assertThat(page.titleField(), is(not(nullValue())));
+        assertThat(page.titleField().getAttribute("title"), is(notNull()));
+        assertThat(page.estimateField(), is(not(nullValue())));
+        assertThat(page.estimateField().getAttribute("estimate"), is(notNull()));
     }
 }
